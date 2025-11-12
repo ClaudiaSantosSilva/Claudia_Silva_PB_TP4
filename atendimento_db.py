0461529c6889a5973afa6e6a981ccf05bb725e18
claudia.tss <claudia.tss@outlook.com>
2025-11-11 21:11:05 -0300

    Atendimento_db com funções em Alchemy corrigidas.

--- atendimento_db.py
+++ atendimento_db.py
@@ -17,53 +17,60 @@ def consultar_produtos_db():
     return produtos
 
 def consultar_cliente_db(id):
-    try:
-        cliente = session.query(Cliente).where(Cliente.id == id).one()
-        return cliente.nome
-    except NoResultFound:
-        print("Necessário realizar cadastro. Cliente não encontrado")
-        realizar_cadastro_cliente()
+    with session:
+        try:
+            #cliente = session.query(Cliente).where(Cliente.id == id).one()
+            cliente = session.query(Cliente).get(id)
+            return cliente.nome
+        except NoResultFound:
+            print("Necessário realizar cadastro. Cliente não encontrado")
+            realizar_cadastro_cliente()
 
 def realizar_cadastro_cliente():
-    maior_id = session.query(func.max(Cliente.id)).scalar() #scalar retorna o valor em si
-    novo_id = 1 if maior_id is None else maior_id + 1
-    nome = f"Cliente{novo_id}"
-    novo_cliente = Cliente(nome)
-    session.add(novo_cliente)
-    session.commit()
-    consultar_cliente_db(novo_id)      
+    with session:
+        maior_id = session.query(func.max(Cliente.id)).scalar() #scalar retorna o valor em si
+        novo_id = 1 if maior_id is None else maior_id + 1
+        nome = f"Cliente{novo_id}"
+        novo_cliente = Cliente(nome)
+        session.add(novo_cliente)
+        session.commit()
+        consultar_cliente_db(novo_id)      
 
 def consultar_produto_db(id):
-    produto = None
-    try:
-       session = conectar()
-       produto = session.query(Produto).get(id)
-    except Exception as ex:
-        print(ex)
-    finally:
-        desconectar(session)
-    return produto
+    with session:
+        produto = session.query(Produto).get(id)
+        return produto
+    # produto = None
+    # try:
+    #    produto = session.query(Produto).get(id)
+    #    #produto = session.query(Produto).where(Produto.id == id).first()
+    #    return produto
+    # except Exception as ex:
+    #     print(ex)     
 
 def alterar_produto_db(nova_quantidade, produto_escolhido):
     #comando = "update produto set quantidade = ? where id = ?;"
-    try:
-        session = conectar()
+    with session:
         session.query(Produto).filter(Produto.id == produto_escolhido.id).update({"quantidade":nova_quantidade})
         session.commit()
-    except Exception as ex:
-        print(ex)
-    finally:
-        desconectar(session)
-
+    # try:        
+    #     session.query(Produto).filter(Produto.id == produto_escolhido.id).update({"quantidade":nova_quantidade})
+    #     session.commit()
+    # except Exception as ex:
+    #     print(ex)
+    
 def consultar_produtos_zerados_db():
     #comando = "select * from produto where quantidade <= 0;"
-    try:
-        session = conectar()
-        produtos_zerados = session.query(Produto).filter(Produto.quantidade <= 0).all() #usando all() já me devolve uma lista
-    except Exception as ex:
-        print(ex)
-    finally:
-        desconectar(session)
-    return produtos_zerados 
+    with session:
+        produtos_zerados = session.query(Produto).filter(Produto.quantidade <= 0).all()
+        return produtos_zerados
+    # try:
+    #     session = conectar()
+    #     produtos_zerados = session.query(Produto).filter(Produto.quantidade <= 0).all() #usando all() já me devolve uma lista
+    # except Exception as ex:
+    #     print(ex)
+    # finally:
+    #     desconectar(session)
+    # return produtos_zerados 
 
 
